c503c1a2a26ba729815d25760599b9ed4776e1fa
Dhanush-Kalvium <marella.reddy.s69@kalvium.community>
2025-01-17 14:42:49 +0530

    added-Login-Page

---
+++ Frontend/src/components/Login.jsx
@@ -0,0 +1,85 @@
+import { useForm } from "react-hook-form";
+
+import { Link } from "react-router-dom";
+
+function Login() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm({
+    defaultValues: {
+      username: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = (data) => {
+    console.log(data);
+    reset();
+  };
+
+  return (
+    <>
+      <div className="login-container flex items-center justify-center min-h-screen bg-gray-100">
+        <div className="logInbox w-full max-w-sm p-6 bg-white rounded-xl shadow-lg">
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <h1 className="text-2xl font-semibold text-center text-gray-700 mb-6">
+              Login to Your Account
+            </h1>
+            <label>Email:</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              className="w-full px-4 py-2 mt-1 text-gray-700 bg-gray-100 border rounded-lg"
+              {...register("username", { required: "Username is required" })}
+            />
+            <br />
+            {errors.username && (
+              <div className="text-red-500 text-xs">
+                {errors.username.message}
+              </div>
+            )}
+            <label>Password:</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              className="w-full px-4 py-2 mt-1 text-gray-700 bg-gray-100 border rounded-lg"
+              {...register("password", { required: "Password is required" })}
+            />
+            <br />
+            {errors.password && (
+              <div className="text-red-500 text-xs">
+                {errors.password.message}
+              </div>
+            )}
+            <div className="flex justify-end">
+              <button className="mt-3  text-gray-700 text-xs">
+                forgot password?
+              </button>
+            </div>
+            <button
+              type="submit"
+              className="m-6 mx-0 w-full px-4 py-2 text-lg font-medium text-white bg-gradient-to-r from-blue-500 to-indigo-500 rounded-lg shadow-md hover:from-blue-600 hover:to-indigo-600"
+            >
+              Login
+            </button>
+            <span className="ml-20 text-gray-700 text-xs">
+              {" "}
+              Do nott have an account?
+            </span>
+            &nbsp;
+            <Link to="/signup" className="text-blue-600 text-xs">
+              Signup
+            </Link>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Login;
---
+++ Frontend/src/components/SignUp.jsx
@@ -0,0 +1,67 @@
+// SignUp.js
+
+const SignUp = () => {
+  return (
+    <div className="h-screen flex justify-center items-center bg-gray-100">
+      <div className="max-w-md w-full bg-white rounded-lg shadow-md p-6">
+        <h2 className="text-lg font-bold mb-4 items-center flex justify-center">
+          Sign Up
+        </h2>
+        <form>
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="email"
+            >
+              Email
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="email"
+              type="email"
+              placeholder="Enter your email"
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="password"
+            >
+              Password
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="password"
+              type="password"
+              placeholder="Enter your password"
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="confirmPassword"
+            >
+              Confirm Password
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="confirmPassword"
+              type="password"
+              placeholder="Confirm your password"
+            />
+          </div>
+          <div className="flex items-center justify-between">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="submit"
+            >
+              Sign Up
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default SignUp;
